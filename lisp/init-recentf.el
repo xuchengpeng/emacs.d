fed134a572032a517230d6622ade7d73b8b0fe66
xuchengpeng <330476629@qq.com>
2018-04-18 09:46:18 +0800

    Add recentf-exclude directory

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -6,7 +6,9 @@
         recentf-max-menu-items 15
         ;; disable recentf-cleanup on Emacs start, because it can cause
         ;; problems with remote files
-        recentf-auto-cleanup 'never)
+        recentf-auto-cleanup 'never
+        recentf-exclude (list "/tmp/" "/ssh:"
+                              (concat user-emacs-directory "elpa/")))
   (recentf-mode +1))
 
 (provide 'init-recentf)
