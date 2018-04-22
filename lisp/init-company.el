6091924da9194bbfc3ec5fdc4d7ac1fcceb2b80b
xuchengpeng <330476629@qq.com>
2018-04-22 09:06:12 +0800

    (global-set-key (kbd "M-/") 'company-yasnippet)

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -6,6 +6,20 @@
         company-tooltip-limit 15
         company-minimum-prefix-length 2
         company-dabbrev-downcase nil)
+  
+  (global-set-key (kbd "M-/") 'company-yasnippet)
+  
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  
+  ;; (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  
   (global-company-mode 1)
   )
 
