4bdbd3023bb376272605316d0e0118468bbe20fe
xuchengpeng <330476629@qq.com>
2018-04-23 17:25:40 +0800

    No longer use yasnippet-snippets package

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -3,8 +3,9 @@
   :defer 10
   :diminish yas-minor-mode
   :config
+  (setq yas-snippet-dirs (list (concat user-emacs-directory "snippets/")))
   (yas-global-mode 1)
-  (use-package yasnippet-snippets :ensure t)
+  ;; (use-package yasnippet-snippets :ensure t)
   )
 
 (provide 'init-yasnippet)
