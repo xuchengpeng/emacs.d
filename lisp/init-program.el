4b8f9f71cd0c24f11840319f1617797e3b498e35
xuchengpeng <330476629@qq.com>
2018-04-25 11:22:07 +0800

    diminish eldoc

--- lisp/init-program.el
+++ lisp/init-program.el
@@ -50,18 +50,19 @@
 (use-package js2-mode
   :ensure t
   :mode "\\.js\\'"
+  :diminish (js2-mode . "JS2")
   :config
   (setq-default js2-basic-offset 2
                 js2-basic-indent 2
                 js2-bounce-indent-p nil
                 js2-mode-show-parse-errors nil
                 js2-mode-show-strict-warnings nil)
-  (setq mode-name "JS2")
   (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
   )
 
 (use-package js2-refactor
   :ensure t
+  :diminish
   :after js2-mode
   :config
   (add-hook 'js2-mode-hook 'js2-refactor-mode)
@@ -79,5 +80,9 @@
   :ensure t
   :mode ("\\.yaml\\'" "\\.yml\\'")
   )
+
+(use-package eldoc
+  :defer t
+  :diminish)
 
 (provide 'init-program)
