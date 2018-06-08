5b4ee56b73f7718a658661e22997b67118a81053
xuchengpeng <330476629@qq.com>
2018-06-08 10:58:59 +0800

    add org-agenda files

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,11 @@
 (use-package org
   :ensure org-plus-contrib
   :mode ("\\.org$" . org-mode)
+  :bind ("C-c a" . org-agenda)
+  :config
+  (defvar dotemacs-org-directory (concat user-emacs-directory "org/"))
+  (setq org-agenda-files (list (concat dotemacs-org-directory "work.org")
+                               (concat dotemacs-org-directory "gtd.org")))
   )
 
 (provide 'init-org)
