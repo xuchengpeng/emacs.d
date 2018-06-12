cfffae486f2f506d1cf364c27d86ef498bd5289d
xuchengpeng <330476629@qq.com>
2018-06-12 17:39:21 +0800

    set TODO keywords

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -7,6 +7,9 @@
   (defvar dotemacs-org-directory (concat user-emacs-directory "org/"))
   (setq org-agenda-files (list (concat dotemacs-org-directory "work.org")
                                (concat dotemacs-org-directory "gtd.org")))
+  (setq org-todo-keywords
+      '((sequence "TODO" "WAITING" "SOMEDAY" "|" "DONE" "CANCELLED")
+        (sequence "LEARN" "TRY" "TEACH" "|" "COMPLETE")))
   )
 
 (provide 'init-org)
