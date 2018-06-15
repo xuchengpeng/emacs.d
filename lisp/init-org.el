b330791906e07460ff762b22b8b5be6dcbbfe01c
xuchengpeng <330476629@qq.com>
2018-06-15 10:26:30 +0800

    Track time

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,7 +12,13 @@
         (sequence "LEARN" "TRY" "TEACH" "|" "COMPLETE")))
   (setq org-log-done 'time
         org-startup-indented t
-        org-startup-folded nil)
+        org-startup-folded nil
+        org-clock-persist t
+        org-clock-persist-file (concat dotemacs-cache-directory "org-clock-save.el")
+        org-clock-in-resume t
+        org-clock-into-drawer t
+        org-log-into-drawer t)
+  (org-clock-persistence-insinuate)
   )
 
 (provide 'init-org)
