92f733469205bb900e7fe4b5b5759173224c46d5
xuchengpeng <330476629@qq.com>
2018-06-15 09:53:15 +0800

    Fast access to TODO states

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,7 +8,7 @@
   (setq org-agenda-files (list (concat dotemacs-org-directory "work.org")
                                (concat dotemacs-org-directory "gtd.org")))
   (setq org-todo-keywords
-      '((sequence "TODO" "WAITING" "SOMEDAY" "|" "DONE" "CANCELLED")
+      '((sequence "TODO(t)" "WAITING(w@/!)" "|" "DONE(d!)" "CANCELLED(c@)")
         (sequence "LEARN" "TRY" "TEACH" "|" "COMPLETE")))
   (setq org-log-done 'time
         org-startup-indented t
