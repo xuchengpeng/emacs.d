0abdbb71579986de29a985d1e89fcd34117d81e3
xuchengpeng <330476629@qq.com>
2018-06-26 19:32:58 +0800

    Update company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -36,17 +36,23 @@
   :diminish company-mode
   :hook (after-init . global-company-mode)
   :bind (("M-/" . company-complete)
-         ("C-c C-y" . company-yasnippet))
+         ("C-c C-y" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("TAB" . company-complete-common-or-cycle)
+         ("<tab>" . company-complete-common-or-cycle)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
   :config
   (setq company-idle-delay 0.2
         company-tooltip-limit 20
+        company-tooltip-align-annotations t
         company-minimum-prefix-length 2
-        company-dabbrev-downcase nil)
-  
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-  (define-key company-active-map [tab] 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case nil
+        company-require-match nil)
   
   (defvar company-mode/enable-yas dotemacs-company-enable-yas
     "Enable yasnippet for all backends.")
