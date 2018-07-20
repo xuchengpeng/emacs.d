20e3975b17466b51ff166c566626cbe311abb5d8
xuchengpeng <330476629@qq.com>
2018-07-20 11:36:16 +0800

    Customize org capture templates.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -80,7 +80,18 @@
                    ;; and that it has a "Hugo Posts" heading. It can even be a
                    ;; symlink pointing to the actual location of all-posts.org!
                    (file+olp "hugo-posts.org" "Hugo Posts")
-                   (function dotemacs-org-hugo-new-subtree-post-capture-template))))
+                   (function dotemacs-org-hugo-new-subtree-post-capture-template)))
+    
+    (add-to-list 'org-capture-templates '("t" "Tasks"))
+    (add-to-list 'org-capture-templates
+                 '("tt" "Task" entry
+                   (file+headline "task.org" "Task")
+                   "* TODO %^{任务名}\n%u\n%a\n" :clock-in t :clock-resume t))
+    (add-to-list 'org-capture-templates
+                 '("tw" "Work Task" entry
+                   (file+headline "work.org" "Work")
+                   "* TODO %^{任务名}\n%u\n%a\n" :clock-in t :clock-resume t))
+    )
   )
 
 (use-package ox-hugo
