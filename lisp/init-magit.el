9fe5486e73192a2c923d615763de5c7d8e79f61b
xuchengpeng <330476629@qq.com>
2018-08-03 17:55:11 +0800

    Customize magit.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -34,13 +34,16 @@
 (use-package magit
   :bind (("C-x g" . magit-status)
          ("C-x M-g" . magit-dispatch-popup))
-  )
+  :config
+  (setq magit-auto-revert-mode nil))
 
 (use-package magithub
   :after magit
+  :preface
+  (setq magithub-dir (concat dotemacs-cache-directory "magithub/"))
   :config
   (magithub-feature-autoinject t)
-  (setq magithub-clone-default-directory "~/.emacs.d/magithub"))
+  (setq magithub-clone-default-directory "~/"))
 
 (provide 'init-magit)
 
